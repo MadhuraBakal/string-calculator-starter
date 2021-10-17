b687fe820a5104550af888f8473da522f367b692
MadhuraBakal <92643277+MadhuraBakal@users.noreply.github.com>
2021-10-17 18:05:28 +0530

    test02 implemented

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -13,7 +13,26 @@ class StringCalculator {
         result = 0;
         customDelimiter = "";
     }
+public String add(String input) {
+         if (numbers.isEmpty())
+            return String.format("%.0f", result);
 
+        if (isInvalidLastCharacterIn(numbers))
+            return "Number expected but EOF found.";
+
+        if (numbers.startsWith(CUSTOM_DELIMITER_PREFIX))
+            numbers = setCustomDelimiter(numbers);
+
+        if (isNewlineAtInvalidPositionIn(numbers))
+            return String.format("Number expected but '\n' found at position %d.", numbers.lastIndexOf('\n'));
+
+        if (containsNegative(numbers).length() > 0)
+            return String.format("Negative not allowed: %s", containsNegative(numbers));
+
+        calculateSumOf(getStringArray(numbers));
+
+        return hasDecimalPlaces() ? printFloat() : printInteger();
+    }
     @Test
     void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
